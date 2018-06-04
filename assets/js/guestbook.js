20a5037fa481d304c2c5f111674aba7f2b5a3181
ssVirth <ssvirth@mail.ru>
2018-06-04 15:51:55 +0300

    show post count if small screen in guestbook

--- assets/js/guestbook.js
+++ assets/js/guestbook.js
@@ -46,7 +46,7 @@ $(function(){
 	$('.quote').on('click', function(){
 		var quoteid = $(this).attr('id');
 		var id = quoteid.substr(5);
-		var user = $(this).parent().parent().parent().children().children().html().trim();
+		var user = $(this).parent().parent().parent().children().children().children().html().trim();
 		var date = $('#hidden-date-' + id).text().trim();
 		var message = $('#hidden-message-' + id).text().trim();
 		var quote_text = '[quote author=' + user + ' date=' + date +' post=' + id + ']\n' + message + '\n[/quote]\n\n';
